dcfd128aaf8aadde45710008c8b189eaa14cb198
EliHorner <eahorner@ncsu.edu>
2024-02-21 11:58:16 -0500

    Add Sobol Indices Calc (from HPC outs) and Plots

---
+++ SobolIndicesCalc.R
@@ -0,0 +1,217 @@
+#Test for different function methods to calculate SFO and STO indices
+library(terra)
+
+#Setup (set to 100 rasters per case to start)
+num_sources <- 5
+source_list <- c('Host', 'IC', 'Weather', 'Par', 'Process')
+samples <- 20
+
+#File path setup
+setupList <- paste0('case', seq(1,32))
+inpath <- "P:/PoPS_Sobol_Analysis/outputs/"
+outpath <- "P:/PoPS_Sobol_Analysis/outputs/Sobol Outputs/"
+
+#Read in Rasters (Warning: Will make a lot of rasters)
+for (i in setupList){
+  fullPath <- paste0(inpath, i, '/pops_runs/')
+  filelist <- list.files(fullPath, pattern = "inf*")
+  sampleslist <- filelist[1:samples]
+  temp <- rast(paste0(fullPath, sampleslist))
+  assign(paste0(i, "Rasts"), temp)
+}
+
+RastsStack <- c(case1Rasts, case2Rasts, case3Rasts, case4Rasts, case5Rasts, case6Rasts, case7Rasts, case8Rasts,
+                case9Rasts, case10Rasts, case11Rasts, case12Rasts, case13Rasts, case14Rasts, case15Rasts, case16Rasts, 
+                case17Rasts, case18Rasts, case19Rasts, case20Rasts, case21Rasts, case22Rasts, case23Rasts, case24Rasts, 
+                case25Rasts, case26Rasts, case27Rasts, case28Rasts, case29Rasts, case30Rasts, case31Rasts, case32Rasts)
+
+mean_temp <- list(mean(get(paste0(setupList[1], 'Rasts'))))
+for (i in 2:length(setupList)){
+  mean_temp <- append(mean_temp, mean(get(paste0(setupList[i], 'Rasts'))))
+}
+MeansStack <- rast(mean_temp)
+
+ValsStack <- global(RastsStack, sum, na.rm = TRUE)
+MeanValsStack <- global(MeansStack, sum, na.rm = TRUE)
+
+#Pre-compute total variance to save time
+sampleVar <- (stdev(RastsStack))^2
+# writeRaster(sampleVar, paste0(outpath, 'sampleVar.tif'))
+valSampleVar <- global(sampleVar, sum, na.rm = TRUE)
+
+# Read from file
+# sampleVar <- terra::rast(paste0(outpath, 'sampleVar.tif'))
+
+#Control Matrix
+out_u_mat <- matrix(0, nrow = num_sources, ncol = samples*(2^num_sources))
+#rows = host, ic, par; cols = samples
+out_u_mat[,1:samples] <-                    c(1,1,1,1,1)
+out_u_mat[,(samples+1):(2*samples)] =       c(1,1,1,1,0)
+out_u_mat[,((2*samples)+1):(3*samples)] =   c(1,1,1,0,1)
+out_u_mat[,((3*samples)+1):(4*samples)] =   c(1,1,1,0,0)
+out_u_mat[,((4*samples)+1):(5*samples)] =   c(1,1,0,1,1)
+out_u_mat[,((5*samples)+1):(6*samples)] =   c(1,1,0,1,0)
+out_u_mat[,((6*samples)+1):(7*samples)] =   c(1,1,0,0,1)
+out_u_mat[,((7*samples)+1):(8*samples)] =   c(1,1,0,0,0)
+out_u_mat[,((8*samples)+1):(9*samples)] =   c(1,0,1,1,1)
+out_u_mat[,((9*samples)+1):(10*samples)] =  c(1,0,1,1,0)
+out_u_mat[,((10*samples)+1):(11*samples)] = c(1,0,1,0,1)
+out_u_mat[,((11*samples)+1):(12*samples)] = c(1,0,1,0,0)
+out_u_mat[,((12*samples)+1):(13*samples)] = c(1,0,0,1,1)
+out_u_mat[,((13*samples)+1):(14*samples)] = c(1,0,0,1,0)
+out_u_mat[,((14*samples)+1):(15*samples)] = c(1,0,0,0,1)
+out_u_mat[,((15*samples)+1):(16*samples)] = c(1,0,0,0,0)
+out_u_mat[,((16*samples)+1):(17*samples)] = c(0,1,1,1,1)
+out_u_mat[,((17*samples)+1):(18*samples)] = c(0,1,1,1,0)
+out_u_mat[,((18*samples)+1):(19*samples)] = c(0,1,1,0,1)
+out_u_mat[,((19*samples)+1):(20*samples)] = c(0,1,1,0,0)
+out_u_mat[,((20*samples)+1):(21*samples)] = c(0,1,0,1,1)
+out_u_mat[,((21*samples)+1):(22*samples)] = c(0,1,0,1,0)
+out_u_mat[,((22*samples)+1):(23*samples)] = c(0,1,0,0,1)
+out_u_mat[,((23*samples)+1):(24*samples)] = c(0,1,0,0,0)
+out_u_mat[,((24*samples)+1):(25*samples)] = c(0,0,1,1,1)
+out_u_mat[,((25*samples)+1):(26*samples)] = c(0,0,1,1,0)
+out_u_mat[,((26*samples)+1):(27*samples)] = c(0,0,1,0,1)
+out_u_mat[,((27*samples)+1):(28*samples)] = c(0,0,1,0,0)
+out_u_mat[,((28*samples)+1):(29*samples)] = c(0,0,0,1,1)
+out_u_mat[,((29*samples)+1):(30*samples)] = c(0,0,0,1,0)
+out_u_mat[,((30*samples)+1):(31*samples)] = c(0,0,0,0,1)
+out_u_mat[,((31*samples)+1):(32*samples)] = c(0,0,0,0,0)
+
+means_out_u_mat <- matrix(0, nrow = num_sources, ncol = length(setupList))
+#Rows = host, ic, par, Cols = Combinations
+means_out_u_mat[,1] =  c(1,1,1,1,1)
+means_out_u_mat[,2] =  c(1,1,1,1,0)
+means_out_u_mat[,3] =  c(1,1,1,0,1)
+means_out_u_mat[,4] =  c(1,1,1,0,0)
+means_out_u_mat[,5] =  c(1,1,0,1,1)
+means_out_u_mat[,6] =  c(1,1,0,1,0)
+means_out_u_mat[,7] =  c(1,1,0,0,1)
+means_out_u_mat[,8] =  c(1,1,0,0,0)
+means_out_u_mat[,9] =  c(1,0,1,1,1)
+means_out_u_mat[,10] = c(1,0,1,1,0)
+means_out_u_mat[,11] = c(1,0,1,0,1)
+means_out_u_mat[,12] = c(1,0,1,0,0)
+means_out_u_mat[,13] = c(1,0,0,1,1)
+means_out_u_mat[,14] = c(1,0,0,1,0)
+means_out_u_mat[,15] = c(1,0,0,0,1)
+means_out_u_mat[,16] = c(1,0,0,0,0)
+means_out_u_mat[,17] = c(0,1,1,1,1)
+means_out_u_mat[,18] = c(0,1,1,1,0)
+means_out_u_mat[,19] = c(0,1,1,0,1)
+means_out_u_mat[,20] = c(0,1,1,0,0)
+means_out_u_mat[,21] = c(0,1,0,1,1)
+means_out_u_mat[,22] = c(0,1,0,1,0)
+means_out_u_mat[,23] = c(0,1,0,0,1)
+means_out_u_mat[,24] = c(0,1,0,0,0)
+means_out_u_mat[,25] = c(0,0,1,1,1)
+means_out_u_mat[,26] = c(0,0,1,1,0)
+means_out_u_mat[,27] = c(0,0,1,0,1)
+means_out_u_mat[,28] = c(0,0,1,0,0)
+means_out_u_mat[,29] = c(0,0,0,1,1)
+means_out_u_mat[,30] = c(0,0,0,1,0)
+means_out_u_mat[,31] = c(0,0,0,0,1)
+means_out_u_mat[,32] = c(0,0,0,0,0)
+
+#Sobol Functions (Original)
+sobolFirstOrder <- function(valuesList, uMat, uSource){
+  sortList <- uMat[uSource,] #row of control matrix associated with source
+  numWith <- mean(valuesList[sortList == 1]) #expectation with source
+  numWithout <- mean(valuesList[sortList == 0]) #expectation without source
+  numerator <- var(c(numWith, numWithout)) #variance of expectation
+  return(numerator/var(valuesList)) #normalize by total variance
+}
+
+sobolTotalOrder <- function(valuesList, uMat, uSource, totalVar){
+  sortMat <- uMat[-uSource,] #rows of control matrix not associated with source
+  uniqueCases <- ncol(unique(sortMat, MARGIN = 2)) #unique cases, number of columns with unique sets and orders of values
+  varlist <- c(rep(0, uniqueCases)) #initialize variance list
+  for(i in 1:uniqueCases){ #for each unique case do
+    varlist[i] <- var(valuesList[apply(sortMat, 2, identical, unique(sortMat, MARGIN = 2)[,i])])
+    #variance of values from identical columns of source modified control matrix by each unique case (set of columns)
+  }
+  numerator <- mean(varlist) #expectation of variances
+  return(1 - (numerator/ totalVar)) #normalize by total variance
+}
+
+sobolFirstOrderRast <- function(rastersList, uMat, uSource, totalVar){
+  sortList <- uMat[uSource,]
+  numWith <- mean(rastersList[[sortList == 1]])
+  numWithout <- mean(rastersList[[sortList == 0]])
+  numerator <- (stdev(numWith, numWithout))^2
+  return(numerator/totalVar)
+}
+
+sobolTotalOrderRast <- function(rastersList, uMat, uSource, totalVar){
+  sortMat <- uMat[-uSource,]
+  r <- rast(nrow = 1073, ncol = 686, ext = ext(RastsStack), crs = crs(RastsStack))
+  uniqueCases <- ncol(unique(sortMat, MARGIN = 2))
+  varRastlist <- c(rep(r, uniqueCases))
+  for(i in 1:uniqueCases){
+    varRastlist[[i]] <- stdev(rastersList[[apply(sortMat, 2, identical, unique(sortMat, MARGIN = 2)[,i])]])^2
+  }
+  numerator <- mean(varRastlist)
+  return(1 - (numerator/ totalVar))
+}
+
+
+#Run Summarized Sobol Analyses (whole study area) Lump Method
+SFOvalsLump <- c(rep(0, num_sources))
+STOvalsLump <- c(rep(0, num_sources))
+
+for(i in 1:num_sources){
+  SFOvalsLump[i] <- sobolFirstOrder(ValsStack$sum, out_u_mat, i)
+  STOvalsLump[i] <- sobolTotalOrder(ValsStack$sum, out_u_mat, i, var(ValsStack$sum))
+}
+
+write.csv(SFOvalsLump, paste0(outpath, 'Lump2k/SFOVals.csv'))
+write.csv(STOvalsLump, paste0(outpath, 'Lump2k/STOVals.csv'))
+
+#Run Sobol Analyses on Rasters (at cell level) Lump Method
+for(i in 1:num_sources){
+  assign(paste0('SFO_lump_', source_list[i]), sobolFirstOrderRast(RastsStack, out_u_mat, i, sampleVar))
+  writeRaster(get(paste0('SFO_lump_', source_list[i])), paste0(outpath, 'Lump2k/', paste0('SFO_lump_', source_list[i], '.tif')))
+}
+
+for(i in 1:num_sources){
+  assign(paste0('STO_lump_', source_list[i]), sobolTotalOrderRast(RastsStack, out_u_mat, i, sampleVar))
+  writeRaster(get(paste0('STO_lump_', source_list[i])), paste0(outpath, 'Lump2k/', paste0('STO_lump_', source_list[i], '.tif')))
+}
+
+# #Method 2: Variance instead of Mean
+# 
+# #Pre compute Variances
+# var_temp <- list(stdev(get(paste0(setupList[1], 'Rasts')))^2)
+# for (i in 2:length(setupList)){
+#   var_temp <- append(var_temp, (stdev(get(paste0(setupList[i], 'Rasts')))^2))
+# }
+# VarStack <- rast(var_temp)
+# 
+# VarValsStack <- global(VarStack, sum, na.rm = TRUE)
+# 
+# #Pre-compute total variance to save time
+# # sampleVar <- (stdev(RastsStack))^2
+# VarVar <- (stdev(VarStack))^2
+# # writeRaster(sampleVar, paste0(outpath, 'sampleVar.tif'))
+# testVar <- global(VarVar, sum, na.rm = TRUE)
+# 
+# #Run Summarized Sobol Analyses (whole study area) Var Method (aka derivatives)
+# SFOvalsVarTest <- c(rep(0, num_sources))
+# STOvalsVarTest <- c(rep(0, num_sources))
+# 
+# for(i in 1:num_sources){
+#   SFOvalsVarTest[i] <- sobolFirstOrder(VarValsStack$sum, means_out_u_mat, i)
+#   STOvalsVarTest[i] <- sobolTotalOrder(VarValsStack$sum, means_out_u_mat, i, testVar$sum)
+# }
+# 
+# write.csv(SFOvalsVarTest, paste0(outpath, 'SFOValsVarTest.csv'))
+# write.csv(STOvalsVarTest, paste0(outpath, 'STOValsVarTest.csv'))
+# 
+# #Run Sobol Analyses on Rasters (at cell level) Var Method
+# for(i in 1:num_sources){
+#   assign(paste0('SFO_var_', source_list[i]), sobolFirstOrderRast(VarStack, means_out_u_mat, i, VarVar))
+# }
+# 
+# for(i in 1:num_sources){
+#   assign(paste0('STO_var_', source_list[i]), sobolTotalOrderRast(VarStack, means_out_u_mat, i, VarVar))
+# }
---
+++ SobolOutputPlotting.R
@@ -0,0 +1,218 @@
+#Plots for New (Lump Method) Results
+library(terra)
+library(RColorBrewer)
+
+# Read in Sobol Rasters
+
+# Read in Predicted Infection Points
+infections <- terra::rast("P:/PoPS_Sobol_Analysis/outputs/Sobol Outputs/MeansStack.tif")
+Minf <- mean(infections)
+sodPoints <- as.points(Minf)
+sodPoints <- sodPoints[sodPoints$mean >= 1]
+
+# Make Color Palette
+pal <- colorRampPalette(brewer.pal(9, 'OrRd'))(25)
+e <- terra::ext(c(380000,400000,4678000,4711000))
+singleWindow <- terra::ext(c(392000, 397000, 4705000, 4708000))
+multiWindow <- terra::ext(c(381000, 390000, 4682000, 4689000))
+
+#Load Rasters
+fHost1k <- rast("P:/PoPS_Sobol_Analysis/outputs/Sobol Outputs/Lump1k/SFO_lump_Host.tif")
+fIC1k <- rast("P:/PoPS_Sobol_Analysis/outputs/Sobol Outputs/Lump1k/SFO_lump_IC.tif")
+fWeather1k <- rast("P:/PoPS_Sobol_Analysis/outputs/Sobol Outputs/Lump1k/SFO_lump_Weather.tif")
+fPar1k <- rast("P:/PoPS_Sobol_Analysis/outputs/Sobol Outputs/Lump1k/SFO_lump_Par.tif")
+fProcess1k <- rast("P:/PoPS_Sobol_Analysis/outputs/Sobol Outputs/Lump1k/SFO_lump_Process.tif")
+
+tHost1k <- rast("P:/PoPS_Sobol_Analysis/outputs/Sobol Outputs/Lump1k/STO_lump_Host.tif")
+tIC1k <- rast("P:/PoPS_Sobol_Analysis/outputs/Sobol Outputs/Lump1k/STO_lump_IC.tif")
+tWeather1k <- rast("P:/PoPS_Sobol_Analysis/outputs/Sobol Outputs/Lump1k/STO_lump_Weather.tif")
+tPar1k <- rast("P:/PoPS_Sobol_Analysis/outputs/Sobol Outputs/Lump1k/STO_lump_Par.tif")
+tProcess1k <- rast("P:/PoPS_Sobol_Analysis/outputs/Sobol Outputs/Lump1k/STO_lump_Process.tif")
+
+fHost2k <- rast("P:/PoPS_Sobol_Analysis/outputs/Sobol Outputs/Lump2k/SFO_lump_Host.tif")
+fIC2k <- rast("P:/PoPS_Sobol_Analysis/outputs/Sobol Outputs/Lump2k/SFO_lump_IC.tif")
+fWeather2k <- rast("P:/PoPS_Sobol_Analysis/outputs/Sobol Outputs/Lump2k/SFO_lump_Weather.tif")
+fPar2k <- rast("P:/PoPS_Sobol_Analysis/outputs/Sobol Outputs/Lump2k/SFO_lump_Par.tif")
+fProcess2k <- rast("P:/PoPS_Sobol_Analysis/outputs/Sobol Outputs/Lump2k/SFO_lump_Process.tif")
+
+tHost2k <- rast("P:/PoPS_Sobol_Analysis/outputs/Sobol Outputs/Lump2k/STO_lump_Host.tif")
+tIC2k <- rast("P:/PoPS_Sobol_Analysis/outputs/Sobol Outputs/Lump2k/STO_lump_IC.tif")
+tWeather2k <- rast("P:/PoPS_Sobol_Analysis/outputs/Sobol Outputs/Lump2k/STO_lump_Weather.tif")
+tPar2k <- rast("P:/PoPS_Sobol_Analysis/outputs/Sobol Outputs/Lump2k/STO_lump_Par.tif")
+tProcess2k <- rast("P:/PoPS_Sobol_Analysis/outputs/Sobol Outputs/Lump2k/STO_lump_Process.tif")
+
+# First Order Plots
+par(mfrow = c(2,3))
+plot(fHost1k, ext = multiWindow, main = 'Host', col = pal, range = c(0,0.2))
+plot(sodPoints, add = TRUE)
+plot(fIC1k, ext = multiWindow, main = 'Initial Conditions', col = pal, range = c(0,0.2))
+plot(sodPoints, add = TRUE)
+plot(fWeather1k, ext = multiWindow, main = 'Weather', col = pal, range = c(0,0.2))
+plot(sodPoints, add = TRUE)
+plot(fPar1k, ext = multiWindow, main = 'Parameter', col = pal, range = c(0,0.2))
+plot(sodPoints, add = TRUE)
+plot(fProcess1k, ext = multiWindow, main = 'Process', col = pal, range = c(0,0.2))
+plot(sodPoints, add = TRUE)
+plot(which.max(c(fHost1k, fIC1k, fWeather1k, fPar1k, fProcess1k)), ext = multiWindow, col = brewer.pal(5, 'Set1'), main = 'Largest Contributing Source')
+plot(sodPoints, add = TRUE)
+par(mfrow = c(1,1))
+
+par(mfrow = c(2,3))
+plot(fHost1k, ext = singleWindow, main = 'Host', col = pal, range = c(0,0.2))
+plot(sodPoints, add = TRUE)
+plot(fIC1k, ext = singleWindow, main = 'Initial Conditions', col = pal, range = c(0,0.2))
+plot(sodPoints, add = TRUE)
+plot(fWeather1k, ext = singleWindow, main = 'Weather', col = pal, range = c(0,0.2))
+plot(sodPoints, add = TRUE)
+plot(fPar1k, ext = singleWindow, main = 'Parameter', col = pal, range = c(0,0.2))
+plot(sodPoints, add = TRUE)
+plot(fProcess1k, ext = singleWindow, main = 'Process', col = pal, range = c(0,0.2))
+plot(sodPoints, add = TRUE)
+plot(which.max(c(fHost1k, fIC1k, fWeather1k, fPar1k, fProcess1k)), ext = singleWindow, col = brewer.pal(5, 'Set1'), main = 'Largest Contributing Source')
+plot(sodPoints, add = TRUE)
+par(mfrow = c(1,1))
+
+par(mfrow = c(2,3))
+plot(fHost1k, ext = e, main = 'Host', col = pal, range = c(0,0.2))
+#plot(sodPoints, add = TRUE)
+plot(fIC1k, ext = e, main = 'Initial Conditions', col = pal, range = c(0,0.2))
+#plot(sodPoints, add = TRUE)
+plot(fWeather1k, ext = e, main = 'Weather', col = pal, range = c(0,0.2))
+#plot(sodPoints, add = TRUE)
+plot(fPar1k, ext = e, main = 'Parameter', col = pal, range = c(0,0.2))
+#plot(sodPoints, add = TRUE)
+plot(fProcess1k, ext = e, main = 'Process', col = pal, range = c(0,0.2))
+#plot(sodPoints, add = TRUE)
+plot(which.max(c(fHost1k, fIC1k, fWeather1k, fPar1k, fProcess1k)), ext = e, col = brewer.pal(5, 'Set1'), main = 'Largest Contributing Source')
+#plot(sodPoints, add = TRUE)
+par(mfrow = c(1,1))
+
+# Total Order Plots
+par(mfrow = c(2,3))
+plot(tHost1k, ext = multiWindow, main = 'Host', col = pal, range = c(0,1))
+#plot(sodPoints, add = TRUE)
+plot(tIC1k, ext = multiWindow, main = 'Initial Conditions', col = pal, range = c(0,1))
+#plot(sodPoints, add = TRUE)
+plot(tWeather1k, ext = multiWindow, main = 'Weather', col = pal, range = c(0,1))
+#plot(sodPoints, add = TRUE)
+plot(tPar1k, ext = multiWindow, main = 'Parameter', col = pal, range = c(0,1))
+#plot(sodPoints, add = TRUE)
+plot(tProcess1k, ext = multiWindow, main = 'Process', col = pal, range = c(0,1))
+#plot(sodPoints, add = TRUE)
+plot(which.max(c(tHost1k, tIC1k, tWeather1k, tPar1k, tProcess1k)), ext = multiWindow, col = brewer.pal(5, 'Set1'), main = 'Largest Contributing Source')
+#plot(sodPoints, add = TRUE)
+par(mfrow = c(1,1))
+
+par(mfrow = c(2,3))
+plot(tHost1k, ext = singleWindow, main = 'Host', col = pal, range = c(0,1))
+plot(sodPoints, add = TRUE)
+plot(tIC1k, ext = singleWindow, main = 'Initial Conditions', col = pal, range = c(0,1))
+plot(sodPoints, add = TRUE)
+plot(tWeather1k, ext = singleWindow, main = 'Weather', col = pal, range = c(0,1))
+plot(sodPoints, add = TRUE)
+plot(tPar1k, ext = singleWindow, main = 'Parameter', col = pal, range = c(0,1))
+plot(sodPoints, add = TRUE)
+plot(tProcess1k, ext = singleWindow, main = 'Process', col = pal, range = c(0,1))
+plot(sodPoints, add = TRUE)
+plot(which.max(c(tHost1k, tIC1k, tWeather1k, tPar1k, tProcess1k)), ext = singleWindow, col = brewer.pal(5, 'Set1'), main = 'Largest Contributing Source')
+plot(sodPoints, add = TRUE)
+par(mfrow = c(1,1))
+
+par(mfrow = c(2,3))
+plot(tHost1k, ext = e, main = 'Host', col = pal, range = c(0,1))
+#plot(sodPoints, add = TRUE)
+plot(tIC1k, ext = e, main = 'Initial Conditions', col = pal, range = c(0,1))
+#plot(sodPoints, add = TRUE)
+plot(tWeather1k, ext = e, main = 'Weather', col = pal, range = c(0,1))
+#plot(sodPoints, add = TRUE)
+plot(tPar1k, ext = e, main = 'Parameter', col = pal, range = c(0,1))
+#plot(sodPoints, add = TRUE)
+plot(tProcess1k, ext = e, main = 'Process', col = pal, range = c(0,1))
+#plot(sodPoints, add = TRUE)
+plot(which.max(c(tHost1k, tIC1k, tWeather1k, tPar1k, tProcess1k)), ext = e, col = brewer.pal(5, 'Set1'), main = 'Largest Contributing Source')
+#plot(sodPoints, add = TRUE)
+par(mfrow = c(1,1))
+
+#Percentage plots
+fSum = fHost1k + fIC1k + fWeather1k + fPar1k + fProcess1k
+tSum = tHost1k + tIC1k + tWeather1k + tPar1k + tProcess1k
+
+fSum2k = fHost2k + fIC2k + fWeather2k + fPar2k + fProcess2k
+tSum2k = tHost2k + tIC2k + tWeather2k + tPar2k + tProcess2k
+
+pctHost2k <- tHost2k / tSum2k
+pctIC2k <- tIC2k / tSum2k
+pctWeather2k <- tWeather2k / tSum2k
+pctPar2k <- tPar2k / tSum2k
+pctProcess2k <- tProcess2k / tSum2k
+
+pctHost1k <- tHost1k / tSum
+pctIC1k <- tIC1k / tSum
+pctWeather1k <- tWeather1k / tSum
+pctPar1k <- tPar1k / tSum
+pctProcess1k <- tProcess1k / tSum
+
+pal_pct <- colorRampPalette(brewer.pal(9, 'GnBu'))(25)
+
+#first order percentages
+par(mfrow = c(2,3))
+plot(fHost1k / fSum, ext = e, main = 'Host %', col = pal_pct, range = c(0,1))
+#plot(sodPoints, add = TRUE)
+plot(fIC1k / fSum, ext = e, main = 'Initial Conditions %', col = pal_pct, range = c(0,1))
+#plot(sodPoints, add = TRUE)
+plot(fWeather1k / fSum, ext = e, main = 'Weather %', col = pal_pct, range = c(0,1))
+#plot(sodPoints, add = TRUE)
+plot(fPar1k / fSum, ext = e, main = 'Parameter %', col = pal_pct, range = c(0,1))
+#plot(sodPoints, add = TRUE)
+plot(fProcess1k / fSum, ext = e, main = 'Process %', col = pal_pct, range = c(0,1))
+#plot(sodPoints, add = TRUE)
+#plot(which.max(c(tHost1k, tIC1k, tWeather1k, tPar1k, tProcess1k)), ext = e, col = brewer.pal(5, 'Set1'), main = 'Largest Contributing Source')
+#plot(sodPoints, add = TRUE)
+par(mfrow = c(1,1))
+
+#2k percentages
+par(mfrow = c(2,3))
+plot(tHost2k / tSum2k, ext = e, main = 'Host %', col = pal_pct, range = c(0,0.5))
+plot(sodPoints, add = TRUE)
+plot(tIC2k / tSum2k, ext = e, main = 'Initial Conditions %', col = pal_pct, range = c(0,0.5))
+plot(sodPoints, add = TRUE)
+plot(tWeather2k / tSum2k, ext = e, main = 'Weather %', col = pal_pct, range = c(0,0.5))
+plot(sodPoints, add = TRUE)
+plot(tPar2k / tSum2k, ext = e, main = 'Parameter %', col = pal_pct, range = c(0,0.5))
+plot(sodPoints, add = TRUE)
+plot(tProcess2k / tSum2k, ext = e, main = 'Process %', col = pal_pct, range = c(0,0.5))
+plot(sodPoints, add = TRUE)
+plot(which.max(c(tHost2k, tIC2k, tWeather2k, tPar2k, tProcess2k)), ext = e, col = brewer.pal(5, 'Set1'), main = 'Largest Contributing Source')
+#plot(sodPoints, add = TRUE)
+par(mfrow = c(1,1))
+
+#1k and 2k differences
+par(mfrow = c(2,3))
+plot(pctHost2k - pctHost1k, ext = multiWindow, main = 'Host % Diff', col = brewer.pal(7, 'Spectral'), range = c(-0.25, 0.25))
+#(sodPoints, add = TRUE)
+plot(pctIC2k - pctIC1k, ext = multiWindow, main = 'Initial Conditions % Diff', col = brewer.pal(7, 'Spectral'), range = c(-0.25, 0.25))
+#plot(sodPoints, add = TRUE)
+plot(pctWeather2k - pctWeather1k, ext = multiWindow, main = 'Weather % Diff', col = brewer.pal(7, 'Spectral'), range = c(-0.25, 0.25))
+#plot(sodPoints, add = TRUE)
+plot(pctPar2k - pctPar1k, ext = multiWindow, main = 'Parameter % Diff', col = brewer.pal(7, 'Spectral'), range = c(-0.25, 0.25))
+#plot(sodPoints, add = TRUE)
+plot(pctProcess2k - pctProcess1k, ext = multiWindow, main = 'Process % Diff', col = brewer.pal(7, 'Spectral'), range = c(-0.25, 0.25))
+#plot(sodPoints, add = TRUE)
+#plot(which.max(c(tHost2k, tIC2k, tWeather2k, tPar2k, tProcess2k)), ext = e, col = brewer.pal(5, 'Set1'), main = 'Largest Contributing Source')
+#plot(sodPoints, add = TRUE)
+par(mfrow = c(1,1))
+
+#Summary Data frame
+vals1k <- data.frame(source = c('Host', 'IC', 'Weather', 'Par', 'Process'), 
+                     first_order_sum = c(global(fHost1k, sum, na.rm = TRUE)$sum, global(fIC1k, sum, na.rm = TRUE)$sum, global(fWeather1k, sum, na.rm = TRUE)$sum, global(fPar1k, sum, na.rm = TRUE)$sum, global(fProcess1k, sum, na.rm = TRUE)$sum), 
+                     total_order_sum = c(global(tHost1k, sum, na.rm = TRUE)$sum, global(tIC1k, sum, na.rm = TRUE)$sum, global(tWeather1k, sum, na.rm = TRUE)$sum, global(tPar1k, sum, na.rm = TRUE)$sum, global(tProcess1k, sum, na.rm = TRUE)$sum),
+                     first_order_mean = c(global(fHost1k, mean, na.rm = TRUE)$mean, global(fIC1k, mean, na.rm = TRUE)$mean, global(fWeather1k, mean, na.rm = TRUE)$mean, global(fPar1k, mean, na.rm = TRUE)$mean, global(fProcess1k, mean, na.rm = TRUE)$mean),
+                     total_order_mean = c(global(tHost1k, mean, na.rm = TRUE)$mean, global(tIC1k, mean, na.rm = TRUE)$mean, global(tWeather1k, mean, na.rm = TRUE)$mean, global(tPar1k, mean, na.rm = TRUE)$mean, global(tProcess1k, mean, na.rm = TRUE)$mean)
+)
+
+vals2k <- data.frame(source = c('Host', 'IC', 'Weather', 'Par', 'Process'), 
+                     first_order_sum = c(global(fHost2k, sum, na.rm = TRUE)$sum, global(fIC2k, sum, na.rm = TRUE)$sum, global(fWeather2k, sum, na.rm = TRUE)$sum, global(fPar2k, sum, na.rm = TRUE)$sum, global(fProcess2k, sum, na.rm = TRUE)$sum), 
+                     total_order_sum = c(global(tHost2k, sum, na.rm = TRUE)$sum, global(tIC2k, sum, na.rm = TRUE)$sum, global(tWeather2k, sum, na.rm = TRUE)$sum, global(tPar2k, sum, na.rm = TRUE)$sum, global(tProcess2k, sum, na.rm = TRUE)$sum),
+                     first_order_mean = c(global(fHost2k, mean, na.rm = TRUE)$mean, global(fIC2k, mean, na.rm = TRUE)$mean, global(fWeather2k, mean, na.rm = TRUE)$mean, global(fPar2k, mean, na.rm = TRUE)$mean, global(fProcess2k, mean, na.rm = TRUE)$mean),
+                     total_order_mean = c(global(tHost2k, mean, na.rm = TRUE)$mean, global(tIC2k, mean, na.rm = TRUE)$mean, global(tWeather2k, mean, na.rm = TRUE)$mean, global(tPar2k, mean, na.rm = TRUE)$mean, global(tProcess2k, mean, na.rm = TRUE)$mean)
+)
